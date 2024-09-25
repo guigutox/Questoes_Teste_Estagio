7382356e146a993a546b0a3e2b7a3f5e80732613
Guilherme <guilhermegutofer@gmail.com>
2024-09-24 22:17:22 -0300

    Upgrade no exercicio 2

--- questao2.js
+++ questao2.js
@@ -13,11 +13,12 @@ function fibonacci(n) {
 
 function verificarFibonacci(numero){
     if (fibonacci(numero)) {
-        console.log("O número digitado é um número de Fibonacci.");
+        console.log("O número "+ numero + " digitado é um número de Fibonacci.");
     } else {
-        console.log("O número digitado não é um número de Fibonacci.");
+        console.log("O número "+ numero +" digitado não é um número de Fibonacci.");
     }
 }
 
 //Defina no parametro o valor a ser passado
+verificarFibonacci(13);
 verificarFibonacci(22);
